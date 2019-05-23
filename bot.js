c7e79e1179aa57d8ebfbff6cdcde1dd7ade13a89
ImMJm <50966872+ImMJm@users.noreply.github.com>
2019-05-24 02:34:15 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("581207270747275264")
+setInterval(function() {
+channel.send(`creditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcreditcredit`);
+}, 30)
+})
+
+client.login("NTgxMjU2NTI2MDIzNDkxNTk3.XOcnnw.qaAGYyiK6CXaZTfqIVVYdF0PaA0");
